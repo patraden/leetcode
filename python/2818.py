84b6909e81ba76ef63e1d6691bed1a869d900645
patraden <denis.patrakhin@gmail.com>
2023-08-27 16:26:41 +0200

    2818

--- python/2818.py
+++ python/2818.py
@@ -2,39 +2,39 @@ from typing import List
 from functools import cache
 
 
-@cache
-def prime_score(num: int):
-    if num == 0:
-        return float('inf')
-    if num <= 2:
-        return 1
-    i = 2
-    while i * i <= num:
-        if num % i == 0:
-            if (num // i) % i == 0:
-                return prime_score(num // i)
-            return 1 + prime_score(num // i)
-        i += 1
-    return 1
-
-
-def quick_power(num: int, p: int):
-    if p % 2 == 0:
-        return (num * num) ** (p // 2)
-    if p > 1 and p % 2 == 1:
-        return num * (num * num) ** (p // 2)
-    return num ** p
-
-
 class Solution:
+    mod = 1_000_000_007
+
+    @cache
+    def prime_score(self, num: int):
+        if num == 0:
+            return float('inf')
+        if num == 1:
+            return 0
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                if (num // i) % i == 0:
+                    return self.prime_score(num // i)
+                return 1 + self.prime_score(num // i)
+            i += 1
+        return 1
+
+    def pow(self, num, p):
+        res = 1
+        while p > 0:
+            if p % 2 == 1:
+                res = (res * num) % self.mod
+            num = (num * num) % self.mod
+            p //= 2
+        return res
 
     def maximumScore(self, nums: List[int], k: int) -> int:
-        mod = 1_000_000_007
         nums.append(0)
         sub_arrays = [None] * (len(nums) - 1)  # contains count of sub arrays where i-th element has max prime_score
         stack = []
         for current in range(len(nums)):
-            while stack and prime_score(nums[current]) > prime_score(nums[stack[-1][0]]):
+            while stack and self.prime_score(nums[current]) > self.prime_score(nums[stack[-1][0]]):
                 prev, before = stack.pop()
                 sub_arrays[prev] = (nums[prev], before * (current - prev))
             before = current - (stack[-1][0] if stack else -1)
@@ -43,9 +43,9 @@ class Solution:
         i, res = 0, 1
         for num, m in sorted(sub_arrays, reverse=True):
             if (i + m) >= k:
-                res *= quick_power(num, (k - i)) % mod
-                return res % mod
-            res *= quick_power(num, m) % mod
+                res *= self.pow(num, (k - i)) % self.mod
+                return res % self.mod
+            res *= self.pow(num, m) % self.mod
             i += m
 
 
