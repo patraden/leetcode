9340c809ca1e2427d4fbab3f62229d665f2cf505
patraden <denis.patrakhin@gmail.com>
2023-09-29 09:47:12 +0200

    2334

--- python/2334.py
+++ python/2334.py
@@ -3,39 +3,25 @@ from typing import List
 
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
-        n = len(nums)
-        left = [-1] * n
-        right = [n] * n
-
+        nums.append(-1)
+        # when new item pops old num
+        # it means that item it popped is no longer minimum for relevant subarray
         stack = []
-        for i in range(n):
-            while stack and nums[i] <= nums[stack[-1]]:
-                stack.pop()
-            if stack:
-                left[i] = stack[-1]
+        for i in range(len(nums)):
+            while stack and nums[i] < nums[stack[-1]]:
+                k = stack.pop()
+                s = -1 if not stack else stack[-1]
+                m = i - s - 1
+                if nums[k] > threshold / m:
+                    return m
             stack.append(i)
-
-        stack = []
-        for i in range(n - 1, -1, -1):
-            while stack and nums[i] <= nums[stack[-1]]:
-                stack.pop()
-            if stack:
-                right[i] = stack[-1]
-            stack.append(i)
-
-        for i in range(n):
-            m = right[i] - left[i] - 1
-            if nums[i] > threshold / m:
-                return m
-
         return -1
 
 
 def main():
     print(Solution().validSubarraySize(nums=[1, 3, 4, 3, 1], threshold=6))
-    # print(Solution().validSubarraySize(nums=[6, 5, 6, 5, 8], threshold=7))
+    print(Solution().validSubarraySize(nums=[6, 5, 6, 5, 8], threshold=7))
     print(Solution().validSubarraySize(nums=[1, 1, 1, 1, 1, 1, 1], threshold=6))
-    # print(Solution().validSubarraySize(nums=[1, 7], threshold=2))
 
 
 if __name__ == "__main__":
