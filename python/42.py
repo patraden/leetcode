2864a8134dc7c1ba9263c2b8ace5a38ff83778bb
patraden <denis.patrakhin@gmail.com>
2023-10-01 13:04:01 +0200

    42 optimized

--- python/42.py
+++ python/42.py
@@ -1,37 +1,22 @@
-def trap(height: list[int]):
-    n = len(height)
-    left = [0] * n
-    stack = [-1]
-    for i in range(0, n):
-        idx = stack[-1]
-        while idx != -1 and height[idx] <= height[i]:
-            stack.pop()
-            idx = stack[-1]
-        left[i] = idx
-        stack.append(i)
+from typing import List
 
-    right = [0] * n
-    stack = [n]
-    for i in range(n - 1, -1, -1):
-        idx = stack[-1]
-        while idx != n and height[idx] <= height[i]:
-            stack.pop()
-            idx = stack[-1]
-        right[i] = idx
-        stack.append(i)
 
-    res = 0
-    for h, l, r in set([(height[i], left[i], right[i]) for i in range(n) if (left[i] != -1 and right[i] != n)]):
-        border = min(height[l], height[r])
-        res += (border - h) * (r - l - 1)
-    return res
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res, stack = 0, []
+        for i in range(len(height)):
+            while stack and height[stack[-1]] <= height[i]:
+                k = stack.pop()
+                if stack:
+                    h = min(height[i], height[stack[-1]])
+                    res += (h - height[k]) * (i - stack[-1] - 1)
+            stack.append(i)
+        return res
 
 
 def main():
-    height1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    height2 = [4, 2, 0, 3, 2, 5]
-    print(trap(height1))
-    print(trap(height2))
+    print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(Solution().trap([4, 2, 0, 3, 2, 5]))
 
 
 if __name__ == "__main__":
