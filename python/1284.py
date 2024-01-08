f97de3bd7a2e7e090da7efef85ccd2b8217e629a
d.patrakhin <denis.patrakhin@propellerads.net>
2024-01-08 22:22:12 +0100

    1590

---
+++ python/1284.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def minFlips(self, mat: List[List[int]]) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
