f439f068a74fe5f3c001d0378d2995fee97ef0d3
d.patrakhin <denis.patrakhin@propellerads.net>
2024-04-08 10:43:09 +0200

    201

--- python/201.py
+++ python/201.py
@@ -8,17 +8,9 @@ def b(num):
 
 
 class Solution:
-    def major_bit(self, num):
-        num |= num >> 1
-        num |= num >> 2
-        num |= num >> 4
-        num |= num >> 8
-        num |= num >> 16
-        return num - (num >> 1)
-
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        bit_left = self.major_bit(left)
-        bit_right = self.major_bit(right)
+        bit_left = b(left)
+        bit_right = b(right)
         if bit_right > bit_left:
             return 0
 
@@ -34,7 +26,7 @@ def test():
     print(Solution().rangeBitwiseAnd(left=5, right=7))
     print(Solution().rangeBitwiseAnd(left=0, right=0))
     print(Solution().rangeBitwiseAnd(left=1, right=2147483647))
-    print(bin(Solution().rangeBitwiseAnd(left=int('11010', 2), right=int('11111', 2))))
+    print(Solution().rangeBitwiseAnd(left=int('11010', 2), right=int('11111', 2)))
 
 
 if __name__ == '__main__':
