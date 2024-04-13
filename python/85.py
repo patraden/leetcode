1b8d4409ec6788efe478ba9f106e888194de8d5c
d.patrakhin <denis.patrakhin@propellerads.net>
2024-04-13 09:21:43 +0200

    85 optimized

--- python/85.py
+++ python/85.py
@@ -3,62 +3,36 @@ from typing import List
 
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        n = len(matrix)
-        m = len(matrix[0]) if len(matrix) > 0 else 0
-        matrix_hist = [[0 for _ in range(n)] for _ in range(m)]
+        n, m = len(matrix), len(matrix[0])
+        res = 0
 
-        for i in range(n):
-            count_one = 0
+        matrix.append(['0'] * m)
+        stacks = [[] for _ in range(m)]
+
+        for i in range(n + 1):
+            prefix = 0
             for j in range(m):
                 if matrix[i][j] == "0":
-                    matrix_hist[j][i] = 0
-                    count_one = 0
+                    prefix = 0
                 else:
-                    count_one += 1
-                    matrix_hist[j][i] = count_one
+                    prefix += 1
+                matrix[i][j] = prefix
 
-        _max = 0
-        for row in matrix_hist:
-            _max = max(_max, self.largestRectangleArea(row))
-
-        return _max
-
-    def largestRectangleArea(self, heights: list[int]):
-        n = len(heights)
-        left = [0] * n
-        stack = [-1]
-        for i in range(n):
-            idx = stack[-1]
-            while idx != -1 and heights[idx] >= heights[i]:
-                stack.pop()
-                idx = stack[-1]
-            left[i] = idx
-            stack.append(i)
-
-        right = [0] * n
-        stack = [n]
-        for i in range(n - 1, -1, -1):
-            idx = stack[-1]
-            while idx != n and heights[idx] >= heights[i]:
-                stack.pop()
-                idx = stack[-1]
-            right[i] = idx
-            stack.append(i)
-
-        res = 0
-        for i in range(n):
-            width = right[i] - left[i] - 1
-            res = max(res, heights[i] * width)
-
+                stack = stacks[j]
+                while stack and matrix[stack[-1][0]][j] >= matrix[i][j]:
+                    (idx, li) = stack.pop()
+                    res = max((i - li - 1) * matrix[idx][j], res)
+                li = -1 if not stack else stack[-1][0]
+                stack.append((i, li))
         return res
 
 
 def main():
-    matrix1 = [["1", "0", "1", "0", "0"],
+    matrix1 = [["1", "0", "1", "1", "0"],
                ["1", "0", "1", "1", "1"],
                ["1", "1", "1", "1", "1"],
-               ["1", "0", "0", "1", "0"]]
-    matrix2 = [["1"]]
+               ["1", "0", "1", "1", "0"]]
+    matrix2 = [["0", "1", "1", "1", "1", "1", "1"]]
 
     print(Solution().maximalRectangle(matrix1))
     print(Solution().maximalRectangle(matrix2))
