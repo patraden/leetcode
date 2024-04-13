ad9edfa6389c0e4ed91cd0e8f842af4b6cd87c2a
d.patrakhin <denis.patrakhin@propellerads.net>
2024-04-13 08:55:35 +0200

    84 optimized

--- python/84.py
+++ python/84.py
@@ -1,38 +1,25 @@
-def largestRectangleArea(heights: list[int]):
-    n = len(heights)
-    left = [0] * n
-    stack = [-1]
-    for i in range(n):
-        idx = stack[-1]
-        while idx != -1 and heights[idx] >= heights[i]:
-            stack.pop()
-            idx = stack[-1]
-        left[i] = idx
-        stack.append(i)
+from typing import List
 
-    right = [0] * n
-    stack = [n]
-    for i in range(n - 1, -1, -1):
-        idx = stack[-1]
-        while idx != n and heights[idx] >= heights[i]:
-            stack.pop()
-            idx = stack[-1]
-        right[i] = idx
-        stack.append(i)
 
-    res = 0
-    for i in range(n):
-        width = right[i] - left[i] - 1
-        res = max(res, heights[i] * width)
-
-    return res
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n, stack = len(heights), []
+        heights.append(float('-inf'))
+        res = 0
+        for i in range(n + 1):
+            while stack and heights[stack[-1][0]] >= heights[i]:
+                (idx, li) = stack.pop()
+                res = max((i - li - 1) * heights[idx], res)
+            li = -1 if not stack else stack[-1][0]
+            stack.append((i, li))
+        return res
 
 
 def main():
     test1 = [2, 1, 5, 6, 2, 3]
-    test2 = [1, 1]
-    print(largestRectangleArea(test1))
-    print(largestRectangleArea(test2))
+    test2 = [2, 4]
+    print(Solution().largestRectangleArea(test1))
+    print(Solution().largestRectangleArea(test2))
 
 
 if __name__ == "__main__":
