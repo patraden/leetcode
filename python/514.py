ff821a4f407bc539815ddce290ca1266c82d7660
d.patrakhin <denis.patrakhin@propellerads.net>
2024-04-27 17:42:24 +0200

    514

---
+++ python/514.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        n, dp = len(ring), {'-1': {0: 0}}
+        for i, c in enumerate(ring):
+            dp.setdefault(c, {})[i] = 0
+
+        pk = '-1'
+        for k in key:
+            for i in dp[k]:
+                dst = float('inf')
+                for j, val in dp[pk].items():
+                    dst = min(dst, val + min((i - j) % n, (j - i) % n))
+                dp[k][i] = dst + 1
+            pk = k
+
+        return min(dp[key[-1]].values())
+
+
+
+def test():
+    # Solution().findRotateSteps(ring="godding", key="gd")
+    print(Solution().findRotateSteps(ring="godding", key="godding"))
+    print(Solution().findRotateSteps(ring="zd", key="zd"))
+
+
+if __name__ == '__main__':
+    test()
