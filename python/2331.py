9e81b8a9e6a0488c14ccd975fdae1ef31cb3eab6
d.patrakhin <denis.patrakhin@propellerads.net>
2024-05-16 07:53:50 +0200

    2331

---
+++ python/2331.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val in (0, 1):
+            return bool(root.val)
+
+        if root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    test()
