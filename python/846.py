913ddfadf579fe203278976246a1dbbb2ddd7f6e
d.patrakhin <denis.patrakhin@propellerads.net>
2024-06-06 20:57:14 +0200

    846 O(n)

--- python/846.py
+++ python/846.py
@@ -8,19 +8,24 @@ class Solution:
             return False
 
         counter = Counter(hand)
-        for card in sorted(counter.keys()):
-            while counter[card]:
-                for i in range(groupSize):
-                    if counter[card + i] == 0:
-                        return False
-                    counter[card + i] -= 1
+        for card in hand:
+            start = card
+            while counter[start - 1]:
+                start -= 1
 
+            while counter[start]:
+                for i in range(groupSize):
+                    if counter[start + i] == 0:
+                        return False
+                    counter[start + i] -= 1
         return True
 
 
 def test():
     print(Solution().isNStraightHand(hand=[1, 2, 3, 6, 2, 3, 4, 7, 8], groupSize=3))
     print(Solution().isNStraightHand(hand=[1, 2, 1, 1, 2, 3, 2, 3, 3], groupSize=3))
+    print(Solution().isNStraightHand(hand=[1, 2, 3, 4, 5, 6], groupSize=2))
+    print(Solution().isNStraightHand(hand=[2, 1], groupSize=2))
 
 
 if __name__ == '__main__':
