419a3a8e767e37ad607d16e107c4d8cfe7bb5700
d.patrakhin <denis.patrakhin@propellerads.net>
2024-06-25 08:33:18 +0200

    1038

---
+++ python/1038.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        prefix = 0
+        stack = [root]
+        current = root
+        while True:
+            if current:
+                current = current.right
+                if current:
+                    stack.append(current)
+            elif stack:
+                current = stack.pop()
+                prefix += current.val
+                current.val = prefix
+                current = current.left
+                if current:
+                    stack.append(current)
+            else:
+                break
+        return root
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    test()
