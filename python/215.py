7ab62d50d9791062364e45830bff3868dd1d3e4f
d.patrakhin <denis.patrakhin@propellerads.net>
2024-07-08 21:44:59 +0200

    215

--- python/215.py
+++ python/215.py
@@ -1,25 +1,57 @@
 from typing import List
-import heapq
+
 
 
 class Solution:
+    # def findKthLargest(self, nums, k):
+    #     if not nums: return
+    #     pivot = random.choice(nums)
+    #     left = [x for x in nums if x > pivot]
+    #     mid = [x for x in nums if x == pivot]
+    #     right = [x for x in nums if x < pivot]
+    #
+    #     L, M = len(left), len(mid)
+    #
+    #     if k <= L:
+    #         return self.findKthLargest(left, k)
+    #     elif k > L + M:
+    #         return self.findKthLargest(right, k - L - M)
+    #     else:
+    #         return mid[0]
+    def partitioning(self, a, lo, hi):
+        import random
+        if lo == hi:
+            return lo
+
+        i = lo
+        # pivot = a[hi]
+        pivot_idx = random.randint(lo, hi)
+        pivot = a[pivot_idx]
+        a[pivot_idx], a[hi] = a[hi], a[pivot_idx]
+
+        for j in range(lo, hi):
+            if a[j] < pivot:
+                a[j], a[i] = a[i], a[j]
+                i += 1
+        a[hi], a[i] = a[i], a[hi]
+        return i
+
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        h = []
-        for num in nums:
-            heapq.heappush(h, -num)
-        i = 0
-        elem = None
-        while i < k:
-            elem = heapq.heappop(h)
-            i += 1
-        if elem is not None:
-            return -elem
+        lo, hi = 0, len(nums) - 1
+
+        while lo <= hi:
+            idx = self.partitioning(nums, lo, hi)
+            if idx == len(nums) - k:
+                return nums[idx]
+            elif idx > len(nums) - k:
+                hi = idx - 1
+            else:
+                lo = idx + 1
 
 
 def test():
-    print(Solution().findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2))
+    print(Solution().findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=1))
     print(Solution().findKthLargest(nums=[3, 2, 3, 1, 2, 4, 5, 5, 6], k=4))
-    print(Solution().findKthLargest(nums=[3, 2, 3, 1, 2, 4, 5, 5, 6], k=0))
 
 
 if __name__ == '__main__':
