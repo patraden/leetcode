d4d79bd30cfab2a0503309a32b5919a860b5d009
d.patrakhin <denis.patrakhin@propellerads.net>
2024-09-18 23:38:22 +0200

    queue

---
+++ go/datastructures/queue/queue.go
@@ -0,0 +1,53 @@
+/*
+fixed size queue based on slice
+*/
+
+package queue
+
+import "fmt"
+
+type Queue struct {
+	queue []any
+	head  int
+	tail  int
+	maxN  int
+	size  int
+}
+
+func NewQueue(n int) *Queue {
+	return &Queue{
+		queue: make([]any, n),
+		head:  0,
+		tail:  0,
+		maxN:  n,
+		size:  0,
+	}
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *Queue) Push(x interface{}) error {
+
+	if q.size == q.maxN {
+		return fmt.Errorf("queue is out of capacity %v", q.maxN)
+	}
+
+	q.queue[q.tail] = x
+	q.tail = (q.tail + 1) % q.maxN
+	q.size += 1
+	return nil
+
+}
+
+func (q *Queue) Pop() interface{} {
+	if q.IsEmpty() {
+		return nil
+	}
+	x := q.queue[q.head]
+	q.queue[q.head] = nil
+	q.head = (q.head + 1) % q.maxN
+	q.size -= 1
+	return x
+}
---
+++ go/datastructures/queue/queue_test.go
@@ -0,0 +1,36 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueue(t *testing.T) {
+	q := NewQueue(3)
+	if q.maxN != 3 {
+		t.Errorf("max size is not set")
+	}
+
+	if !q.IsEmpty() {
+		t.Errorf("queue expected to be empty")
+	}
+
+	q.Push(1)
+	q.Push(2)
+	assert.Equal(t, q.Push(3), nil, "Unexpected capacity error")
+	assert.Error(t, q.Push(4), "Unexpected capacity error")
+	assert.Equal(t, q.Pop(), 1)
+	assert.Equal(t, q.Pop(), 2)
+
+	q.Push(4)
+	q.Push(5)
+	assert.Equal(t, q.Pop(), 3)
+	assert.Equal(t, q.Pop(), 4)
+	assert.Equal(t, q.Pop(), 5)
+
+	if !q.IsEmpty() {
+		t.Errorf("queue expected to be empty")
+	}
+
+}
