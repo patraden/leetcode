a4c55231162373952cf724ed2d722ea6fc2653a6
d.patrakhin <denis.patrakhin@propellerads.net>
2026-04-11 20:28:03 +0200

    feat: 3741 optimized

--- go/problems/3741.go
+++ go/problems/3741.go
@@ -2,21 +2,28 @@ package problems
 
 func minimumDistance(nums []int) int {
 	res := -1
-	m := map[int][]int{}
+	m := map[int][3]int{}
 
 	for i, num := range nums {
-		m[num] = append(m[num], i)
-		if len(m[num]) < 3 {
+		if _, ok := m[num]; !ok {
+			m[num] = [3]int{-1, -1, i}
 			continue
 		}
 
-		n := len(m[num])
+		v := m[num]
+		v[0], v[1], v[2] = v[1], v[2], i
+		m[num] = v
+
+		if v[0] == -1 {
+			continue
+		}
+
 		if res == -1 {
-			res = (m[num][n-1] - m[num][n-3]) * 2
+			res = (v[2] - v[0]) * 2
 			continue
 
 		}
-		res = min(res, (m[num][n-1]-m[num][n-3])*2)
+		res = min(res, (v[2]-v[0])*2)
 	}
 
 	return res
